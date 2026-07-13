d9037fc061b7980697433ca6c70c75d4a41eca6c
AntoshinaE <elsheb@yandex.ru>
2026-07-13 17:25:53 +0300

    new

---
+++ src/test/java/FeeCalculationTests1.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class FeeCalculationTests1 {
+    @Test
+    void test01Open() {
+        open("https://slqa.ru/cases/fc/v01/index.php");
+        sleep(3_000);
+        $(By.name("sum")).sendKeys("100");
+        $(By.name("submit")).click();
+        $(By.name("sum")).setValue("200");
+        $(By.name("submit")).click();
+        $(By.name("sum")).type("300");
+        $(By.name("submit")).click();
+             sleep(3_000);
+    }
+}
